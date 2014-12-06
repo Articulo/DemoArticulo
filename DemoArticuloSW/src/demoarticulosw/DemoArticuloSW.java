4245a478c3810bce18a76dd1093222e265ab811d
PC-71 <PC-71@Estudiante>
2014-12-05 21:26:17 -0400

    ya lo hice

--- DemoArticuloSW/src/demoarticulosw/DemoArticuloSW.java
+++ DemoArticuloSW/src/demoarticulosw/DemoArticuloSW.java
@@ -21,6 +21,7 @@ public class DemoArticuloSW {
         int i=0;
         i=1;
         System.out.println("Funko tropa de lagartos");
+        System.out.println("Ya logre enviarlo");
     }
     
 }
